7e8c0aa5b85b825385948f4e06e60d67a54190cd
Marikamiki <118117702+Marikamiki@users.noreply.github.com>
2022-12-10 17:20:53 +0300

    add if else

---
+++ ex005ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "marika")
+{
+    Console.WriteLine("Hi, Chuvyrla");
+}
+else 
+{
+    Console.Write("Hi,");
+    Console.WriteLine(username);
+}
